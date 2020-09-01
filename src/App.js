b4d677ae4dd7b896580e91cef245a43a2155167b
Cameron Goode <cgoode101@gmail.com>
2020-08-31 22:00:09 -0400

    put Menu in a container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import { Container } from "react-bootstrap";
+
 import { Menu } from "./components/Menu";
 
 function App() {
   return (
     <div className="App">
-      Hello, OrderPal
-      <Menu />
+      <Container>
+        <Menu />
+      </Container>
     </div>
   );
 }
