951b71a59c61ca04d1f1c1a2723e482dcaeb5ba1
Christine <christineayoh9@gmail.com>
2020-08-31 22:18:08 -0400

    temporary link to pending order page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Pending from "./payment/pending-order.js";
+import Receipt from "./payment/receipt.js";
+import Payment from "./payment/payment-portal.js";
 
 function App() {
   return (
-    <div className="App">
-      Hello, OrderPal
-    </div>
+    <Router>
+      <div className="App">
+        Hello, OrderPal
+        <div>
+          <Link to="/pending">
+            <button>Go to Pending Order</button>
+          </Link>
+        </div>
+        <Route path="/pending" component={Pending} />
+		<Route path="/receipt" component={Receipt} />
+		<Route path="/payment" component={Payment} />
+      </div>
+    </Router>
   );
 }
 
