010d6c5927c7023030478a940652be42d5fd1362
Cameron Goode <cgoode101@gmail.com>
2020-09-01 17:42:33 -0400

    put orderstate in App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Container } from "react-bootstrap";
 
 import { Menu } from "./components/Menu";
 
 function App() {
+  /**
+   * Global order state that holds the user's order as an array of menu item objects
+   */
+  const [order, setOrder] = useState([]);
   return (
     <div className="App">
       <Container>
-        <Menu />
+        <Menu setOrder={setOrder} order={order} />
       </Container>
     </div>
   );
